b3d4fdce0b95ec895ae63399ededeaad00fce33e
Rennan Ribas <rennanrr@hotmail.com.com>
2025-06-15 07:57:31 -0300

    fix: main layout back with new patterns

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import { LoadSpinner } from './components/common'
 // Main App Layout Component following Container/Presentational pattern
 export default function App() {
   return (
-    <div className='min-h-screen flex flex-col bg-bg-primary'>
+    <div className='layout'>
       <Header />
-      <main className='flex-1 pt-16'>
-        <Suspense 
+      <main className='main-content'>
+        <Suspense
           fallback={
             <div className='flex items-center justify-center h-64'>
               <LoadSpinner size='lg' variant='primary' />
